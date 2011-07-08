fa71c92b70f70405f9432c2663dbb852f13ae030
Matthew Sherborne <msherborne@gmail.com>
2011-07-08 23:46:22 +1000

    More compile options

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-g++ -o cba2csv cba2csv.cpp -O3 -l podofo -l freetype -l fontconfig -l pthread -l jpeg && ./cba2csv test.pdf
+# My home machine
+#g++ -o cba2csv cba2csv.cpp -O3 -march=barcelona -DNDEBUG -l podofo -l freetype -l fontconfig -l pthread -l jpeg && ./cba2csv test.pdf
+
+# General (all cpus) and optimized
+g++ -o cba2csv cba2csv.cpp -O3 -DNDEBUG -l podofo -l freetype -l fontconfig -l pthread -l jpeg && ./cba2csv test.pdf
+
+# Debug
+#g++ -o cba2csv cba2csv.cpp -O0 -g -DDEBUG -l podofo -l freetype -l fontconfig -l pthread -l jpeg && ./cba2csv test.pdf
